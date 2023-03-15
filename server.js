a5fb025f4f197002281db97275f905dd91dbffee
daveS <davesimpcontrol@gmail.com>
2023-03-14 19:51:35 -0600

    routes added

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ const api = require('./public/assets/js/index')
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () =>
-  console.log(`check it out! http://localhost:${PORT}`)
-);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/api', api);
 
+app.get("/", (req, res) =>
+res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`check it out! http://localhost:${PORT}`)
+);
