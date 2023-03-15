f6a6569c6a9400d435fd2ded42218c52f23ad0d7
daveS <davesimpcontrol@gmail.com>
2023-03-14 19:28:58 -0600

    added dependencies

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const api = require('./public/assets/js/index')
+
 const PORT = process.env.PORT || 3001;
-const express = require('express')
 
 app.listen(PORT, () =>
   console.log(`check it out! http://localhost:${PORT}`)
