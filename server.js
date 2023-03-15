6c8ee203bc3ee52e5bd7ae100a2caadc656923a8
daveS <davesimpcontrol@gmail.com>
2023-03-14 19:42:14 -0600

    added middleware

--- server.js
+++ server.js
@@ -10,3 +10,9 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>
   console.log(`check it out! http://localhost:${PORT}`)
 );
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', api);
+
