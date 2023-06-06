51190c20e7f1d6f95c03cecfb66c03fdb6d971b1
daveS <davesimpcontrol@gmail.com>
2023-06-05 23:04:41 -0600

    answered all comments

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,73 @@ const { v4: uuidv4 } = require("uuid");
 
 const router = express.Router();
 
-// Read notes from db.json file
+// Read notes from db
 function readNotesFromFile(callback) {
-  // Read the contents of the db.json file
-  // Parse the data as JSON
-  // Pass the notes to the callback function
+  // Read db content
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return callback(err);
+    }
+
+    // get data as a JSON
+    const notes = JSON.parse(data);
+
+    // Pass the notes to the callback function
+    return callback(null, notes);
+  });
 }
 
-// Write notes to db.json file
+// Write notes to db
 function writeNotesToFile(notes, callback) {
-  // Write the notes array to the db.json file
-  // Invoke the callback function to indicate the operation is complete
+  // Write the notes array to the db
+  fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+    if (err) {
+      console.error(err);
+      return callback(err);
+    }
 
+    console.log("File saved!");
+
+    // callback to indicate the operation is complete
+    return callback(null);
+  });
 }
 
 // READ route
 router.get("/notes", (req, res) => {
-  // Retrieve all notes from the file
-  // If an error occurred while reading the file, return a 500 error
-  // Return the notes as a JSON response
+  // Retrieve all notes
+  readNotesFromFile((err, notes) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+
+    console.log("Notes:", notes);
+    console.log("Type:", typeof notes);
+
+    // Return the notes as a JSON
+    return res.json(notes);
+  });
 });
 
 // CREATE route
 router.post("/notes", (req, res) => {
   console.log("Req Body:", req.body);
   console.log("Req Body Type:", typeof req.body);
-  // If the request body does not contain a title or text, return a 400 error
+
+  const { title, text } = req.body;
+
+  if (!title || !text) {
+    // If the request body does not contain a title or text, return a 400 error
+    return res.status(400).json({ error: "Empty Strings are insufficent" });
+  }
+
   // Retrieve existing notes from the file
   readNotesFromFile((err, notes) => {
+    if (err) {
       // If an error occurred while reading the file, return a 500 error
-    })
+      return res.status(500).json({ error: "Internal server error" });
+    }
 
     // Create a new note object with a unique ID
     const newNote = {
@@ -53,23 +90,27 @@ router.post("/notes", (req, res) => {
         return res.status(500).json({ error: "Internal server error" });
       }
 
-      // Return a 201 status indicating the note was successfully created
+      // Return a 201 when the noteis created
       return res.status(201).json({ message: "Note Created" });
     });
   });
-
+});
 
 // DELETE route
 router.delete("/notes/:id", (req, res) => {
   console.log("Req Params:", req.params);
 
-  // Retrieve existing notes from the file
+  // Retrieve existing notes
   readNotesFromFile((err, notes) => {
     if (err) {
       // If an error occurred while reading the file, return a 500 error
       return res.status(500).json({ error: "Internal server error" });
     }
-    // Filter out the note with the specified ID
+
+    // Filter out the note with using its ID
+    const filteredNotes = notes.filter((note) => note.id !== req.params.id);
+    console.log("Filtered Notes:", filteredNotes);
+
     // Write the filtered notes back to the file
     writeNotesToFile(filteredNotes, (err) => {
       if (err) {
@@ -79,7 +120,7 @@ router.delete("/notes/:id", (req, res) => {
 
       console.log("deleted note");
 
-      // Return a 200 status indicating the note was successfully deleted
+      // Return a 200 status when a note is deleted
       return res.status(200).json({ message: "Note Deleted" });
     });
   });
